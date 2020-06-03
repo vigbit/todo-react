f8f212692528a1d3f09625370d8427b6edc6b7ab
vigbit <vigneshb@live.com>
2020-06-03 09:03:40 +0800

    "delete feature"

--- src/client/Form.jsx
+++ src/client/Form.jsx
@@ -9,13 +9,14 @@ class Form extends React.Component {
     super(props);
     this.state = {
       item: '',
-      itemArr: []
+      itemArr: [],
 
     };
-
+    this.removeItemArr = this.removeItemArr.bind(this)
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
+
 
   handleChange(event){
     this.setState({item: event.target.value});
@@ -25,11 +26,24 @@ class Form extends React.Component {
     event.preventDefault();
     this.setState({
       item: '',
-      itemArr: [this.state.item, ...this.state.itemArr ]
+      itemArr: [...this.state.itemArr, this.state.item ]
     })
 
   }
 
+  // removeItemArr(name, i){
+  //   let itemArr = this.state.itemArr.slice();
+  //   itemArr.splice(i, 1);
+  //   this.setState({
+  //       itemArr
+  //   });
+  // }
+
+  removeItem(name){
+    this.setState({
+        todo: this.state.todo.filter(el => el !== name)
+    })
+}
 
   render() {
     return (
@@ -39,11 +53,29 @@ class Form extends React.Component {
             <input type="text" minLength={5} maxLength={200} value={this.state.item} onChange={this.handleChange} />
           <button>Submit</button>
         </form>
-        <List itemArr={this.state.itemArr}/>
+        <List itemArr={this.state.itemArr} removeItemArr={this.removeItemArr}/>
 
       </div>
     );
   }
 }
+
+class TodoList extends React.Component {
+
+    removeItem(item, i) {
+        this.props.removeItemArr(item, i);
+    }
+
+    render() {
+        return(
+            <ul>
+                { this.props.itemArr.map((todo,i) => {
+                    return <li onClick={() => { this.removeItem(todo, i)}} key={i}>{ todo }</li>
+                })}
+            </ul>
+        );
+    }
+}
+
 
 export default Form;
